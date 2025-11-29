6eea26f637801dbed420c4fe814fc0b7fb84379a
Ricardo-Ceia <A49000@alunos.isel.pt>
2025-11-29 20:02:31 +0000

    retrieve the user id

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
-
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,39 +9,46 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func main(){
-	err := godotenv.Load()
-	
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
+type User struct {
+	Data []struct {
+		ID string `json:"id"`
+	} `json:"data"`
+}
 
+func main(){
+	godotenv.Load()
+	
 	access_token := os.Getenv("TWITCH_ACCESS_TOKEN")
 	client_id := os.Getenv("TWITCH_CLIENT_ID")
-	fmt.Println(access_token)
-	fmt.Println(client_id)
+	username := "ronaldomadeir" 	
+	req, err := http.NewRequest("GET", "https://api.twitch.tv/helix/users?login=" + username, nil)
 	
-	req,	err := http.NewRequest("GET","https://api.twitch.tv/helix/users?login=twitchdev",nil)
-	
-	if err!=nil{
+	if err != nil {
 		log.Fatal(err)
 	}
-
-	bearerString := fmt.Sprintf("Bearer %s",access_token)
-	req.Header.Add("Authorization",bearerString)
-	req.Header.Add("Client-Id",client_id)
+	
+	bearerString := fmt.Sprintf("Bearer %s", access_token)
+	req.Header.Add("Authorization", bearerString)
+	req.Header.Add("Client-Id", client_id)
+	
 	client := &http.Client{}
-	res,	err := client.Do(req)
-
-	if err!=nil{
+	res, err := client.Do(req)
+	if err != nil {
 		log.Fatal(err)
 	}
-
-	body,	err := io.ReadAll(res.Body)
-	if err!=nil{
-		log.Fatal(err)
-	}
-
-	fmt.Println("res:",string(body))
 	defer res.Body.Close()
+	
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var user User
+
+	err = json.Unmarshal(body,&user)
+	if err!=nil{
+		log.Fatal(err)
+	}
+
+	user_id := user.Data[0].ID
+	fmt.Println("id:",user_id)
 }
