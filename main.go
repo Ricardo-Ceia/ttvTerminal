57960954aa3d1efb4f42d4d7e95bf1111b94dd88
Ricardo-Ceia <A49000@alunos.isel.pt>
2025-11-29 19:46:51 +0000

    make get request to twitch Api

--- main.go
+++ main.go
@@ -2,8 +2,46 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
+	"io"
+	"os"
+	"github.com/joho/godotenv"
 )
 
 func main(){
-	fmt.Println("Hello world!")
+	err := godotenv.Load()
+	
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	access_token := os.Getenv("TWITCH_ACCESS_TOKEN")
+	client_id := os.Getenv("TWITCH_CLIENT_ID")
+	fmt.Println(access_token)
+	fmt.Println(client_id)
+	
+	req,	err := http.NewRequest("GET","https://api.twitch.tv/helix/users?login=twitchdev",nil)
+	
+	if err!=nil{
+		log.Fatal(err)
+	}
+
+	bearerString := fmt.Sprintf("Bearer %s",access_token)
+	req.Header.Add("Authorization",bearerString)
+	req.Header.Add("Client-Id",client_id)
+	client := &http.Client{}
+	res,	err := client.Do(req)
+
+	if err!=nil{
+		log.Fatal(err)
+	}
+
+	body,	err := io.ReadAll(res.Body)
+	if err!=nil{
+		log.Fatal(err)
+	}
+
+	fmt.Println("res:",string(body))
+	defer res.Body.Close()
 }
